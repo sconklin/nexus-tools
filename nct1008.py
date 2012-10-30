2725662e1d041d458a7507fa01530c65fda4def7
Steve Conklin <sconklin@canonical.com>
2012-10-30 14:56:22 +0100

    Add license and copyright

--- nct1008.py
+++ nct1008.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python
 #
 #
-# This defines filter sets which are applied to the metadata in benchmark results in order to filter them for charting.
-#
+# Copyright (C) Canonical, Inc 2012
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 class NCT1008:
 
